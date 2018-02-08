649ac29ec6b2d437ab36e790b4984d2b0ffb9f94
giovane <giovane.tomaz@redealumni.com>
2018-02-08 03:21:09 -0200

    [WIP] Start Asana wrapper

---
+++ src/asana.js
@@ -0,0 +1,16 @@
+'use strict';
+
+import asana from 'asana';
+
+class Asana {
+
+  constructor(accessToken) {
+    if (!accessToken) {
+      throw new Error("No Asana access token provided. For more information: https://github.com/giovanecosta/magic-wand#how-to-get-asana-access-token");
+    }
+    this.client = asana.Client.create().useAccessToken(accessToken);
+  }
+
+}
+
+module.exports = { Asana }
---
+++ test/asana.js
@@ -0,0 +1,23 @@
+'use strict';
+
+import { expect } from 'chai';
+
+import asana from 'asana';
+import { Asana } from '../src/asana';
+
+describe('#Asana Tools', function() {
+
+  context('create Object', function() {
+    it('should create Asana Client', function() {
+      const asanaApi = new Asana('access_token_qwerty_lorem_ipsum');
+
+      expect(asanaApi.client).to.be.an.instanceof(asana.Client);
+    });
+
+    it('should broke without access token', function() {
+      expect(() => new Asana()).to.throw(/No Asana access token provided/);
+    });
+
+  });
+
+});
