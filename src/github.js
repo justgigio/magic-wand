05a1d2af89bf704952e23e32be02dd5c53c1a3df
ftuyama <felipetuyama@gmail.com>
2018-02-19 01:26:04 -0300

    Add more features

--- src/github.js
+++ src/github.js
@@ -5,6 +5,7 @@
 //
 // Docs:  https://github.com/octokit/rest.js
 //        https://octokit.github.io/rest.js/
+//        https://developer.github.com/v3/pulls/#create-a-pull-request
 //
 // ## Usage:
 //
@@ -20,24 +21,40 @@ require('dotenv').config()
 class GitHub {
 
   constructor() {
-    this.client = new github();
+    this.client = new github({
+      debug: true
+    });
     this.client.authenticate({
       type: 'oauth',
-      key: process.env.CLIENT_ID,
-      secret: process.env.CLIENT_SECRET
+      token: process.env.TOKEN
     });
     this.setRepo(process.env.REPO_OWNER, process.env.REPO);
   }
 
   setRepo(owner, repo) {
-    this.current_owner = owner;
-    this.current_repo = repo;
+    this.current = {"owner": owner, "repo": repo };
   }
 
-  pullRequests() {
-    this.client.pullRequests.getAll({"owner": this.current_owner, "repo": this.current_repo}).then(result => { this.pull_requests = result; })
+  async pullRequests() {
+    this.pull_requests = await this.client.pullRequests.getAll(this.current);
   }
 
+  async pullRequest(number) {
+    let params = Object.assign({}, this.current, {"number": number});
+    this.pull_request = await this.client.pullRequests.get(params);
+  }
+
+  async createPullRequest(head, base="master", title="PR") {
+    let params = Object.assign({}, this.current, {"head": head, "base": base, "title": title});
+    this.pull_request = await this.client.pullRequests.create(params);
+  }
+
+  async requestReview(reviewers) {
+    if (typeof this.pull_request !== "undefined") {
+      let params = Object.assign({}, this.current, {"number": this.pull_request.data.number, "reviewers": reviewers});
+      this.reviewrs = await this.client.pullRequests.createReviewRequest(params)
+    }
+  }
 }
 
 module.exports = { GitHub }
